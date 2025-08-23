02ff029038ea51826ff9a5eb20641d399925053e
Saleh Yahya <salehyahya10.20@gmail.com>
2025-08-23 11:07:15 -0500

    Working on function page list

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -13,18 +13,7 @@ app.post ("/process-video", (req, res) => {
         res.status(400).send("Bad Request: Missing file path.");
     }
 
-    
-        ffmpeg(inputFilePath)
-        .outputOptions('-vf', 'scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2') //720p /*forceOrginalAspect... is just a force command to make sure there is no distortion*/
-        .on("end", () => { /*async event listener */
-            res.status(200).send("Processing finished successfully.")
-        })
-        .on("error", (err) => {
-            console.log(`An error occured: ${err.message}`);
-            res.status(500).send(`Internal Server Error: ${err.message}`)
-        })
-        .save(outputFilePath); /*Save the output file and specifing the path*/
-    });
+});
 //========================================================= Server Starting ============================================================================================================
 const port = process.env.PORT || 3000; /*this is a standard norm to providfe the port at runtime*/
 app.listen(port, () => { /*this API endpoint allows us to start our sever on our locally hosted comptuter*/   /*() is a callback function*/
--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -6,12 +6,61 @@ import { Storage } from "@google-cloud/storage";
 import fs from "fs";
 import ffmpeg from "fluent-ffmpeg";
 
+const storage = new Storage() /*Creating an instance of storage so we can interact with that API*/
+const  rawVideoBucketName = "sy-raw-videos" /*Downlaod from this bucket*/ 
+const processedVideoBucketName = "sy-processed-videos" /*Upload to this bucke*/
+
+const localRawVideoPath = "./sy-raw-videos" /*the downloaded raw video uplaods will be placed within this folder*/
+const localProcessedVideoPath = "./sy-processed-videos" /*the processed vidoes will be placed within this folder*/
+
 
 export function setupDirectories() { /*Creates the local dependencies for raw AND processed videos */
 }
 
+
 /** this is the conversion code, hence where we take the raw video and process it to the video 720p video quality
- * * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}
-* @param processedVideoname - The name of the file to convert to {@link localProcessedVideoPath}
+* @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}
+* @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}
 * @returns A promise that resolves when the video has been converted.
 */
+export function convertVideo(rawVideoName: string, processedVideoName: string) { /*this function is what changes the video quality to 720p*/ 
+    return new Promise<void>((resolve, reject) => { /*wrapping ffmpeg into a promise so we let the clinet know whats going on whikle ffmeg is asyncing*/ 
+        ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+        .outputOptions('-vf', 'scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2') //720p /*forceOrginalAspect... is just a force command to make sure there is no distortion*/
+        .on("end", () => { /*async event listener */
+        console.log("Processing finished successfully.");
+        resolve();
+    })
+        .on("error", (err) => {
+        console.log(`An error occured: ${err.message}`);
+    })
+        .save(`${localProcessedVideoPath}/${processedVideoName}`); /*Save the output file and specifing the path*/
+    })
+
+}
+
+/** 
+* @param fileName - The name of the file to download from the 
+* {@link rawVideoBucketName} bucket into the {@link localProcessedVideoPath} folder
+* @returns A promise that resolves when the video has been converted.
+*/
+
+export async function dowlocalRawVideo(fileName: string) {
+    await storage.bucket(rawVideoBucketName) /*await blocks any console.log() code from running until rawVideoBucketName is stored within bucket */ 
+        .file(fileName)
+        .download({ destination: `${localRawVideoPath}/${fileName}` });
+
+    console.log(
+        `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}} `
+    )
+}
+
+/** 
+* @param fileName - The name of the file to download from the 
+* {@link localProcessedVideoPath} folder into the {@link processedVideoBucketName}
+* @returns A promise that resolves when the video has been uploaded.
+*/
+
+export async function uploadProcessedVideo(fileName: string) {
+
+}
